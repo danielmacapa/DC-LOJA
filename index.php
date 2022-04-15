167de0f179a53031fd5be9ff8d57be446c7cf0ad
danielmacapa <71198712+danielmacapa@users.noreply.github.com>
2022-04-14 21:56:16 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$produto = new Produto();
+$produto->setDescricao(“Produto 01”);
+$produto->setPreco_venda(9.99);
+$produto->setPreco_aluguel(3.99);
+$produto->setQuantidade(3);
+$produto->adicionar();
+
+
+
+
+
+?>
